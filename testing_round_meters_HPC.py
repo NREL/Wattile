33e4f84885affd151c46a7262b085294476a1646
Rob Buechler <buechler@umich.edu>
2020-08-18 12:27:34 -0600

    changed to multiprocessing module instead of system os

--- testing_round_meters_HPC.py
+++ testing_round_meters_HPC.py
@@ -10,19 +10,20 @@ meters = [
     ["ESIF", "ESIF Office Power (kW)"],
     ["RSF2", "RSF2 Mechanical Power (kW)"],
     ["RSF2", "RSF2 Lighting Power (kW)"],
+    ["SSEB", "SSEB Mechanical Power (kW)"]
     ]
 
 #     ["ESEB", "ESEB Main Power (kW)"],
 #     ["TTF", "TTF Main Power (kW)"],
 #     ["SERF", "SERF Main Power (kW)"],
 #     ["FTLB", "FTLB Main Power (kW)"],
-#     ["SSEB", "SSEB Mechanical Power (kW)"],
 #     ["RSF", "RSF Main Power (kW)"],
 #     ["RSF", "RSF Data Center Power (kW)"],
 #     ["S&TF", "S&TF Main Power (kW)"],
 #     ["EC", "EC Main Power (kW)"]
 # ]
 test_ID = "MP_test"
+processes = list()
 
 # Run tests
 for meter_ID in meters:
@@ -37,15 +38,25 @@ for meter_ID in meters:
     configs["building"] = meter_ID[0]
     configs["target_var"] = meter_ID[1]
 
-    # Rewrite configs to disk:
-    with open("configs.json", 'w') as fp:
-        json.dump(configs, fp, indent=1)
+    # # Rewrite configs to disk:
+    # with open("configs.json", 'w') as fp:
+    #     json.dump(configs, fp, indent=1)
 
     # Execute
-    target = configs["target_var"].replace(" ", "").replace("(", "-").replace(")", "")
-    python_file = "/projects/intelcamp20/repos/intelligentcampus-pred-analytics/entry_point.py"
-    stdout_file = "/projects/intelcamp20/repos/intelcamp20-hpc/results/Outputs/stdout_{}.txt".format(configs["target_var"].replace(" ", ""))
-    os.system("python -u " + python_file + " > " + stdout_file + " &")
+    # target = configs["target_var"].replace(" ", "").replace("(", "-").replace(")", "")
+    # python_file = "/projects/intelcamp20/repos/intelligentcampus-pred-analytics/entry_point.py"
+    # stdout_file = "/projects/intelcamp20/repos/intelcamp20-hpc/results/Outputs/stdout_{}.txt".format(configs["target_var"].replace(" ", ""))
+    # os.system("python -u " + python_file + " > " + stdout_file + " &")
+
+    if __name__ == '__main__':
+        p = Process(target=epb.main, args=(configs,))
+        processes.append(p)
+        p.start()
 
     print("Started {}".format(configs["target_var"]))
-    #print("Just finished training for target variable {}".format(meter_ID[1]))
+
+print("Joining Processes...")
+for p in processes:
+    p.join()
+print("Done")
+
