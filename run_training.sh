ae7ca0ce66777d51b4ea9ae820de27eb49789a50
Rob Buechler <buechler@umich.edu>
2021-09-22 12:23:25 -0600

    Included shell file to run GPU job on Eagle

--- run_training.sh
+++ run_training.sh
@@ -1,24 +1,20 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=12
-#SBATCH --cpus-per-task=3
-##SBATCH --mem=184000
-#SBATCH --time=0-00:59:00
+#SBATCH --time=0-02:00:00
+#SBATCH --gres=gpu:1
 #SBATCH --account=intelcamp21
 #SBATCH --mail-user=robert.buechler@nrel.gov
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --output=/projects/intelcamp21/repos/intelcamp20-hpc/results/Outputs/slurm-%j.out
-#SBATCH --job-name=BGP_batch_test
+#SBATCH --job-name=Cafe_GPU
 
 module purge
 module load conda
-source activate /projects/intelcamp21/conda/oldpandas
+source activate /projects/intelcamp21/conda/pytorch_gpu
 ##conda list
 
-python -u /projects/intelcamp21/repos/intelligentcampus-pred-analytics/testing_round_GP_HPC.py > /projects/intelcamp21/repos/intelcamp20-hpc/results/Outputs/testing_round_${SLURM_JOB_ID}.txt
-##python -u /projects/intelcamp21/repos/intelligentcampus-pred-analytics/entry_point.py
-##python -u /projects/intelcamp21/repos/intelligentcampus-pred-analytics/get_GP_data.py > /projects/intelcamp21/repos/intelcamp20-hpc/results/Outputs/python_output_${SLURM_JOB_ID}.txt
-
+python -u /projects/intelcamp21/repos/intelligentcampus-pred-analytics/entry_point.py > /projects/intelcamp21/repos/intelcamp20-hpc/results/Outputs/testing_round_${SLURM_JOB_ID}.txt
+tensorboard dev upload --logdir /projects/intelcamp21/repos/intelcamp20-hpc/results/RNN_MCafeWholeBuildingRealPowerTotal_T7-9-21 --one_shot
 
 echo "Done with sbatch script"
 
