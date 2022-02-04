53b11b9faad0e801741263e36b4c0b1e03cd746e
Hannah Eslinger <heslinge@nrel.gov>
2022-02-03 18:22:55 -0600

    Replace intelcamp21 with intelcamp

--- run_training.sh
+++ run_training.sh
@@ -2,19 +2,19 @@
 #SBATCH --nodes=1
 #SBATCH --time=0-02:00:00
 #SBATCH --gres=gpu:1
-#SBATCH --account=intelcamp21
+#SBATCH --account=intelcamp
 #SBATCH --mail-user=robert.buechler@nrel.gov
 #SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --output=/projects/intelcamp21/repos/intelcamp20-hpc/results/Outputs/slurm-%j.out
+#SBATCH --output=/projects/intelcamp/repos/intelcamp20-hpc/results/Outputs/slurm-%j.out
 #SBATCH --job-name=Cafe_GPU
 
 module purge
 module load conda
-source activate /projects/intelcamp21/conda/pytorch_gpu
+source activate /projects/intelcamp/conda/pytorch_gpu
 ##conda list
 
-python -u /projects/intelcamp21/repos/intelligentcampus-pred-analytics/entry_point.py > /projects/intelcamp21/repos/intelcamp20-hpc/results/Outputs/testing_round_${SLURM_JOB_ID}.txt
-tensorboard dev upload --logdir /projects/intelcamp21/repos/intelcamp20-hpc/results/RNN_MCafeWholeBuildingRealPowerTotal_T7-9-21 --one_shot
+python -u /projects/intelcamp/repos/intelligentcampus-pred-analytics/entry_point.py > /projects/intelcamp/repos/intelcamp20-hpc/results/Outputs/testing_round_${SLURM_JOB_ID}.txt
+tensorboard dev upload --logdir /projects/intelcamp/repos/intelcamp20-hpc/results/RNN_MCafeWholeBuildingRealPowerTotal_T7-9-21 --one_shot
 
 echo "Done with sbatch script"
 
