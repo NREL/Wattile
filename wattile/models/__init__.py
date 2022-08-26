7ebbd14af64d799b5346aa7e1911ca381d28e635
Hannah Eslinger <heslinge@nrel.gov>
2022-08-26 10:43:38 -0600

    Make charlie work like the other models (#151)
    * Make Test
    
    * Make make Charlie a class
    
    * Add constructor
    
    * Use wattile data processing
    
    * Don't test for training stat

--- wattile/models/__init__.py
+++ wattile/models/__init__.py
@@ -1,4 +1,5 @@
 from wattile.models.alfa_model import AlfaModel
 from wattile.models.bravo_model import BravoModel
+from wattile.models.charlie_model import CharlieModel
 
-MODELS_DICT = {"alfa": AlfaModel, "bravo": BravoModel}
+MODELS_DICT = {"alfa": AlfaModel, "bravo": BravoModel, "charlie": CharlieModel}
