056ffceaaed62475b9232e9e6b179a299a3ab092
Kim <janghyun.kim@nrel.gov>
2022-08-31 11:58:13 -0600

    Update buildings_processing.py

--- wattile/buildings_processing.py
+++ wattile/buildings_processing.py
@@ -116,7 +116,7 @@ def input_data_split(data, configs):
     """
     # setting configuration parameters
     arch_version = configs["arch_version"]
-    file_prefix = Path(configs["data_handling"]["end_time"])
+    file_prefix = Path(configs["data_handling"]["exp_dir"])
     mask_file = os.path.join(file_prefix, "mask.h5")
 
     # assign timestamp and data size depending on arch_version
