5da0967574e331cc720e3baeffe63a76abf37f17
Kim <janghyun.kim@nrel.gov>
2022-09-27 08:35:57 -0600

    update on test

--- tests/integration_tests/test_prediction.py
+++ tests/integration_tests/test_prediction.py
@@ -94,8 +94,7 @@ def test_prediction_bravo(config_for_tests, tmpdir):
         data_processing_configs["input_output_window"]["window_width_target"]
     )
     resample_interval = pd.Timedelta(data_processing_configs["resample_interval"])
-    secondary_num = data_processing_configs["input_output_window"]["secondary_num"]
-    num_timestamps = (window_width_target // resample_interval) + secondary_num
+    num_timestamps = window_width_target // resample_interval
 
     assert results.shape[1:] == (
         len(config_for_tests["learning_algorithm"]["quantiles"]),
