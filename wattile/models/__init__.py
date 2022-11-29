e548afea3f3e31ef9c429baae72d24b82beb4ac4
Hannah Eslinger <heslinge@nrel.gov>
2022-11-29 13:48:03 -0700

    Address comments

--- wattile/models/__init__.py
+++ wattile/models/__init__.py
@@ -2,9 +2,6 @@ from wattile.error import ConfigsError
 from wattile.models.alfa_model import AlfaModel
 from wattile.models.AlgoMainRNNBase import AlgoMainRNNBase
 from wattile.models.bravo_model import BravoModel
-from wattile.models.charlie_model import CharlieModel
-
-MODELS_DICT = {"alfa": AlfaModel, "bravo": BravoModel, "charlie": CharlieModel}
 
 
 class ModelFactory:
