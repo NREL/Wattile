3b080e1d257b88a3436a7db0a68612a962129c1c
Kim <janghyun.kim@nrel.gov>
2023-01-10 11:05:33 -0700

    Revert "Update data_reading.py"
    This reverts commit 25554fd4e1558b19724a53d840fed1b8f8655be3.

--- wattile/data_reading.py
+++ wattile/data_reading.py
@@ -103,13 +103,13 @@ def read_dataset_from_file(configs):
     # the extra will be removed in `prep_for_rnn`
     timestamp_start = dt.datetime.fromisoformat(configs["data_input"]["start_time"])
     timestamp_end = dt.datetime.fromisoformat(configs["data_input"]["end_time"])
-    if configs["data_processing"]["resample"]["bin_label"] == "left":
+    if configs["data_processing"]["resample"]["bin_closed"] == "left":
         df_inputdata = df_inputdata.loc[
             (df_inputdata.start < timestamp_end)
             & (df_inputdata.end >= timestamp_start),
             :,
         ]
-    elif configs["data_processing"]["resample"]["bin_label"] == "right":
+    elif configs["data_processing"]["resample"]["bin_closed"] == "right":
         df_inputdata = df_inputdata.loc[
             (df_inputdata.start <= timestamp_end)
             & (df_inputdata.end > timestamp_start),
